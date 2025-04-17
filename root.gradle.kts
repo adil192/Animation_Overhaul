b687921abd650ea2b00e671ac5021d3eaa9eedb2
Adil Hanney <adilhanney@disroot.org>
2025-04-18 00:29:44 +0100

    fix(start links at 1.21 fabric)

--- root.gradle.kts
+++ root.gradle.kts
@@ -6,16 +6,16 @@ plugins {
 }
 
 preprocess {
-    val forge12101 = createNode("1.21.1-forge", 12101, "yarn")
     val fabric12101 = createNode("1.21.1-fabric", 12101, "yarn")
-    val fabric12001 = createNode("1.20.1-fabric", 12001, "yarn")
+    val forge12101 = createNode("1.21.1-forge", 12101, "yarn")
     val forge12001 = createNode("1.20.1-forge", 12001, "yarn")
-    val forge11902 = createNode("1.19.2-forge", 11902, "yarn")
+    val fabric12001 = createNode("1.20.1-fabric", 12001, "yarn")
     val fabric11902 = createNode("1.19.2-fabric", 11902, "yarn")
+    val forge11902 = createNode("1.19.2-forge", 11902, "yarn")
 
-    forge12101.link(fabric12101)
-    fabric12101.link(fabric12001)
-    fabric12001.link(forge12001)
-    forge12001.link(forge11902)
-    forge11902.link(fabric11902)
+    fabric12101.link(forge12101)
+    forge12101.link(forge12001)
+    forge12001.link(fabric12001)
+    fabric12001.link(fabric11902)
+    fabric11902.link(forge11902)
 }
